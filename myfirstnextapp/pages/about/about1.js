9eb8299f9e61f23ad9ca5fc09591bc2cbde1ac92
gpunit2417 <punitgoyal106@gmail.com>
2025-01-25 21:44:04 +0530

    nested routing files for about folder

---
+++ myfirstnextapp/pages/about/about1.js
@@ -0,0 +1,9 @@
+const about1 = () => {
+  return (
+    <div>
+        <h1>Hello from about1.js file</h1>
+    </div>
+  )
+}
+
+export default about1
---
+++ myfirstnextapp/pages/about/about2.js
@@ -0,0 +1,9 @@
+const about2 = () => {
+  return (
+    <div>
+        <h1>Hello from about2.js file</h1>
+    </div>
+  )
+}
+
+export default about2
