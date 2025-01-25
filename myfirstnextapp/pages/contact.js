bc1a0f6dfeef50dd10daa91ad6fe895c14d62fd0
gpunit2417 <punitgoyal106@gmail.com>
2025-01-25 21:31:37 +0530

    contact.js file and contacts function

---
+++ myfirstnextapp/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const contacts = () => {
+  return (
+    <div>
+        <h1>Hello from contact.js file with function name contacts</h1>
+    </div>
+  )
+}
+
+export default contacts
