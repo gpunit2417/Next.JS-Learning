a847cd52bf40d5c9abf058a444146dd5301171b7
gpunit2417 <punitgoyal106@gmail.com>
2025-01-25 21:30:52 +0530

    about folder's index.js file

---
+++ myfirstnextapp/pages/about/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const index = () => {
+  return (
+    <div>
+        <h1>Hello from about folder with file named index.js</h1>
+    </div>
+  )
+}
+
+export default index
