7c9fce15189803c6a1a7c544d6fd26198bed2e96
gpunit2417 <punitgoyal106@gmail.com>
2025-01-26 09:43:57 +0530

    imported navbar

--- myfirstnextapp/pages/about/index.js
+++ myfirstnextapp/pages/about/index.js
@@ -1,8 +1,10 @@
+import Navbar from '@/components/Navbar'
 import React from 'react'
 
 const index = () => {
   return (
     <div>
+      <Navbar/>
         <h1>Hello from about folder with file named index.js</h1>
     </div>
   )
