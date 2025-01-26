bd887afd10a486bec526289f791147e53a0627c1
gpunit2417 <punitgoyal106@gmail.com>
2025-01-26 09:43:43 +0530

    refactored code

--- myfirstnextapp/pages/index.js
+++ myfirstnextapp/pages/index.js
@@ -1,37 +1,9 @@
-import Link from "next/link"
+import Navbar from "@/components/Navbar"
 
 const index = () => {
   return (
     <div>
-      <nav>
-        <ul className="menu-bar">
-          <li>
-            <Link href="/" legacyBehavior>
-              <a> Home </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about" legacyBehavior>
-              <a> About </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/contact" legacyBehavior>
-              <a> Contact </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/blog" legacyBehavior>
-              <a> Blog </a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/product" legacyBehavior>
-              <a> Product </a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+      <Navbar/>
     </div>
   )
 }
