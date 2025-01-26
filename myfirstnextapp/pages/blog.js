5c4b08cf28f8f6f6cd077c1405e5c21ff7b491e7
gpunit2417 <punitgoyal106@gmail.com>
2025-01-26 10:40:59 +0530

    internal image

--- myfirstnextapp/pages/blog.js
+++ myfirstnextapp/pages/blog.js
@@ -1,11 +1,12 @@
 import Navbar from '@/components/Navbar'
 import React from 'react'
-
+import Image from 'next/image'
 const blog = () => {
   return (
     <div>
       <Navbar/>
         <h1>Hello from blog.js file</h1>
+        <Image src="/practiceImage.webp" layout='fill'/>
     </div>
   )
 }
