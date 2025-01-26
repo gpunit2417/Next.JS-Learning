e15333c45fa9853247308233012d028705440529
gpunit2417 <punitgoyal106@gmail.com>
2025-01-26 11:20:53 +0530

    fetching of API

--- myfirstnextapp/pages/blog.js
+++ myfirstnextapp/pages/blog.js
@@ -1,8 +1,22 @@
 import Navbar from '@/components/Navbar'
 import React from 'react'
-import Image from 'next/image'
+// import Image from 'next/image'
 import Head from 'next/head'
-const blog = () => {
+import Link from 'next/link';
+
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await res.json();
+
+  return {
+    props: {
+      data,
+    },
+  };
+};
+
+
+const blog = ({data}) => {
   return (
     <div style={{textAlign: 'center'}}>
       <Head>
@@ -11,7 +25,18 @@ const blog = () => {
       <Navbar/>
         <h1>Hello from blog.js file</h1>
         {/* <Image src="/practiceImage.webp" layout='fill'/> */}
-        <Image src="https://images.pexels.com/photos/257897/pexels-photo-257897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" height="300" width="500"/>
+        {/* <Image src="https://images.pexels.com/photos/257897/pexels-photo-257897.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260" height="300" width="500"/> */}
+
+        {data.map((curElem) => {
+        return (
+          <div key={curElem.id} className="ssr-styles">
+            <h3>{curElem.id}</h3>
+            <Link href={`/blog/${curElem.id}`}>
+              <h2>{curElem.title}</h2>
+            </Link>
+          </div>
+        );
+      })}
     </div>
   )
 }
