87c8033bf33250d946125842312ec9ea22c8d097
gpunit2417 <punitgoyal106@gmail.com>
2025-01-26 07:31:21 +0530

    created a navbar section

--- myfirstnextapp/pages/index.js
+++ myfirstnextapp/pages/index.js
@@ -1,7 +1,37 @@
+import Link from "next/link"
+
 const index = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <nav>
+        <ul className="menu-bar">
+          <li>
+            <Link href="/" legacyBehavior>
+              <a> Home </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about" legacyBehavior>
+              <a> About </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/contact" legacyBehavior>
+              <a> Contact </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog" legacyBehavior>
+              <a> Blog </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/product" legacyBehavior>
+              <a> Product </a>
+            </Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }
